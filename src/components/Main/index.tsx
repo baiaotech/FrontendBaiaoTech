445f500981174719b00a72c79e4dd72f21b2b578
mario-leandro <marioleandro2004@gmail.com>
2025-03-17 17:33:38 -0300

    Separando códigos extensos da main - V1.5

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,42 +1,12 @@
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
-import { categorias } from "@/components/categorias";
-import Image from "next/image";
+import CategoriasCarousel from "@/components/CategoriasCarousel";
 
 export default function Main() {
   return (
     <main className="w-full flex justify-center items-start bg-stone-950">
-      <div className="container h-full flex flex-col justify-center items-center">
-        {/* carrossel das categorias */}
-        <div className="w-full flex flex-col justify-center items-center">
-          <div className="w-full h-16 flex justify-start items-center">
-            <p className="text-2xl text-white font-semibold">Categorias</p>
-          </div>
+      <div className="w-7xl h-full flex flex-col justify-start items-start p-3">
+        <CategoriasCarousel />
 
-          <Carousel className="w-full h-35 bg-slate-600 flex justify-center items-center">
-            <CarouselContent>
-              {categorias.map((i) => (
-                <CarouselItem
-                  key={i.id}
-                  className="w-auto h-20 flex flex-col justify-center items-center border-2 bg-[#333] md:basis-1/2 lg:basis-1/3"
-                >
-                  <Image 
-                    src={i.icone} 
-                    alt="icone" 
-                  />
-                  <p>{i.nome}</p>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious className="bg-black text-orange-400" />
-            <CarouselNext className="bg-black text-orange-400" />
-          </Carousel>
-        </div>
+        
       </div>
     </main>
   );
