3a10ac874b149f10fc551c9c9f85f525fa686e8b
mario-leandro <marioleandro2004@gmail.com>
2025-03-19 11:40:02 -0300

    Adicionando mais 2 links

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -15,24 +15,25 @@ export default function Header() {
   };
 
   return (
-    <header className="w-full h-20 bg-[#333] flex justify-center items-center">
-      <div className="w-full h-full px-5 flex flex-row justify-between items-center">
+    <header className="bg-[#333]">
+      <nav className="w-full h-20 flex flex-row justify-between items-center p-6 lg:px-8">
         <div className="h-full flex justify-center items-center">
           <Link
             href="/"
-            className="text-2xl font-bold leading-relaxed text-white uppercase tracking-wider"
+            className="text-2xl font-bold leading-relaxed text-white uppercase"
           >
             Baião <span className="text-orange-500">Tech</span>
           </Link>
         </div>
 
-        <div
-          className={`h-full flex flex-row justify-between items-center gap-8 ${
-            menuOpen ? "hidden" : "flex"
-          }`}
-        >
+        <div className="h-full flex flex-row justify-center items-center gap-8">
+          <Link className="text-white text-xs font-bold uppercase" href="/">Comunidades</Link>
+          <Link className="text-white text-xs font-bold uppercase" href="/">Baião Tech Fest</Link>
+        </div>
+
+        <div className="h-full flex flex-row justify-center items-center gap-8">
           <div className="h-full flex justify-center items-center">
-            <button className="bg-none border-none text-white text-sm font-bold uppercase tracking-wider cursor-pointer flex justify-center items-center">
+            <button className="bg-none border-none text-white text-xs font-bold uppercase cursor-pointer flex justify-center items-center">
               <Image src={locationIcon} alt="Icone de Localização" width={14} />
               <span className="mx-1">Qualquer Lugar</span>
               <Image
@@ -46,46 +47,54 @@ export default function Header() {
           <div className="h-full flex justify-center items-center">
             <Link
               href="/"
-              className="text-orange-500 text-sm font-bold uppercase tracking-wider cursor-pointer"
+              className="text-orange-500 text-xs font-bold uppercase cursor-pointer"
             >
               INDIQUE UM EVENTO
             </Link>
           </div>
         </div>
+      </nav>
 
-        <div className="md:hidden flex justify-center items-center cursor-pointer">
-          <button
-            id="menuOpen"
-            onClick={handleMenuOpen}
-            className="w-7 h-7 bg-none border-none"
-          >
-            <Image
-              src={menuIcon}
-              alt="Menu Icon"
-              className="w-full h-full transition-all"
-            />
-          </button>
+      <div className="md:hidden flex justify-center items-center cursor-pointer">
+        <button
+          id="menuOpen"
+          onClick={handleMenuOpen}
+          className="w-7 h-7 bg-none border-none"
+        >
+          <Image
+            src={menuIcon}
+            alt="Menu Icon"
+            className="w-full h-full transition-all"
+          />
+        </button>
 
-          <button
-            id="menuClose"
-            onClick={handleMenuOpen}
-            className={`w-7 h-7 bg-none border-none ${
-              menuOpen ? "flex" : "hidden"
-            }`}
-          >
-            <Image
-              src={closeMenuIcon}
-              alt="Close menu Icon"
-              className="w-full h-full transition-all"
-            />
-          </button>
-        </div>
+        <button
+          id="menuClose"
+          onClick={handleMenuOpen}
+          className={`w-7 h-7 bg-none border-none ${
+            menuOpen ? "flex" : "hidden"
+          }`}
+        >
+          <Image
+            src={closeMenuIcon}
+            alt="Close menu Icon"
+            className="w-full h-full transition-all"
+          />
+        </button>
+      </div>
+
+      <div
+          className={`h-full flex flex-row justify-between items-center gap-8 ${
+            menuOpen ? "hidden" : "flex"
+          }`}
+        >
+
       </div>
 
       {menuOpen && (
         <div className="md:hidden w-full absolute right-0 top-20 bg-gray-800 z-50 flex flex-col justify-center items-center p-5 gap-5">
           <div className="h-full flex justify-center items-center">
-            <button className="bg-none border-none text-white text-sm font-bold uppercase tracking-wider cursor-pointer flex justify-center items-center">
+            <button className="bg-none border-none text-white text-sm font-bold uppercase cursor-pointer flex justify-center items-center">
               <Image src={locationIcon} alt="Icone de Localização" width={14} />
               <span className="mx-1">Qualquer Lugar</span>
               <Image
@@ -99,7 +108,7 @@ export default function Header() {
           <div className="h-full flex justify-center items-center">
             <Link
               href="/"
-              className="text-orange-500 text-sm font-bold uppercase tracking-wider cursor-pointer"
+              className="text-orange-500 text-sm font-bold uppercase cursor-pointer"
             >
               INDIQUE UM EVENTO
             </Link>
