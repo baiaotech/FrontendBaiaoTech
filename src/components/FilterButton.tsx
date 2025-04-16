ee19792518533a7e63f9fbb1ae8a570108854a0c
mario-leandro <marioleandro2004@gmail.com>
2025-04-16 15:58:20 -0300

    Adicionando botao de filtragem e componentes para adicionar mais filtros futuramente

---
+++ src/components/FilterButton.tsx
@@ -0,0 +1,37 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { categorias } from "@/components/categorias";
+
+export default function FilterButton() {
+  return (
+    <div className="min-w-54 h-auto absolute top-66 bg-[#e6e6e7] rounded-2xl p-3">
+      <Accordion className="w-full min-h-auto" type="single" collapsible>
+        <AccordionItem value="item-1">
+          <AccordionTrigger className="text-base font-semibold">
+            Categorias
+          </AccordionTrigger>
+          <AccordionContent>
+            <ul className="w-full h-full">
+              {categorias.map((i) => (
+                <li
+                  className="w-full flex flex-row justify-start items-center gap-2 text-sm"
+                  key={i.id}
+                >
+                  <input
+                    className="w-4 h-4 accent-slate-900"
+                    type="checkbox"
+                  />
+                  {i.genero}
+                </li>
+              ))}
+            </ul>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
+  );
+}
--- src/components/Main/mainEventosPage.tsx
+++ src/components/Main/mainEventosPage.tsx
@@ -7,10 +7,12 @@ import SkeletonCard from "@/components/skeletonCard";
 import Image from "next/image";
 import { pegarTodosEventos } from "@/routes/api.routes";
 import Link from "next/link";
+import FilterButton from "@/components/FilterButton";
 
 export default function MainEventosPage() {
   const [eventos, setEventos] = useState<Evento[]>([]);
   const [carregando, setCarregando] = useState(true);
+  const [filterBtnOpen, setFilterBtnOpen] = useState(false);
 
   const buscarEventos = async () => {
     setCarregando(true);
@@ -29,6 +31,59 @@ export default function MainEventosPage() {
     buscarEventos();
   }, []);
 
+  const handleFilterButton = () => {
+    setFilterBtnOpen(!filterBtnOpen);
+  };
+
+  const renderSkeleton = () =>
+    Array.from({ length: 3 }).map((_, index) => <SkeletonCard key={index} />);
+
+  const renderEventos = () =>
+    eventos.map((evento) => (
+      <Link
+        href={`/eventos/${evento.id}`}
+        className="w-72 min-h-64 shadow flex flex-col rounded-2xl"
+        key={evento.id}
+      >
+        <div className="w-full h-[150px] bg-orange-500 rounded-t-2xl">
+          <Image
+            className="size-full object-cover rounded-t-2xl"
+            src={evento.cover_photo_url}
+            alt={evento.titulo}
+            width={300}
+            height={300}
+          />
+        </div>
+
+        <div className="w-full flex flex-col justify-between items-start p-4 rounded-b-2xl">
+          <div className="w-full h-auto mb-2">
+            <h3 className="text-xs text-orange-500 text-left font-semibold capitalize mb-2">
+              {evento.data}
+            </h3>
+
+            <h3 className="text-sm text-slate-900 text-left font-semibold capitalize mb-2">
+              {evento.titulo}
+            </h3>
+          </div>
+
+          <div className="w-full max-h-8">
+            <h3 className="text-xs text-slate-900 text-left font-semibold capitalize">
+              {evento.local}
+            </h3>
+          </div>
+        </div>
+      </Link>
+    ));
+
+  const renderNenhumEvento = () =>
+    eventos.length === 0 && (
+      <div className="w-full h-40 flex justify-center items-center">
+        <h1 className="text-2xl font-bold text-slate-900">
+          Nenhum evento encontrado
+        </h1>
+      </div>
+    );
+
   return (
     <main className="w-full flex justify-center items-start p-5">
       <div className="container max-w-7xl h-full flex flex-col justify-start items-start">
@@ -43,10 +98,10 @@ export default function MainEventosPage() {
           </div>
 
           {eventos.length > 0 && (
-            <div className="w-full flex flex-row justify-between mt-5">
+            <div className="w-full flex flex-row justify-between items-center mt-5">
               <div className="min-w-56 h-10 flex justify-start items-center">
                 <input
-                  className="w-96 h-10 text-base font-medium bg-[#e6e6e7] border-r-1 border-slate-400 placeholder:font-bold rounded-l-lg p-2"
+                  className="w-52 h-10 md:w-96 p-1 md:p-2 text-base font-medium bg-[#e6e6e7] border-r-1 border-slate-400 placeholder:font-bold rounded-l-lg"
                   type="text"
                   placeholder="Buscar"
                 />
@@ -56,67 +111,29 @@ export default function MainEventosPage() {
               </div>
 
               <div className="min-w-20 h-10 flex justify-center items-center">
-                <button className="w-32 h-10 text-base font-semibold bg-[#e6e6e7] hover:bg-orange-500 transition rounded-2xl shadow flex justify-center items-center gap-2">
+                <button
+                  className="w-16 md:w-32 h-10 text-base font-semibold bg-[#e6e6e7] hover:bg-orange-500 transition rounded-2xl shadow flex justify-center items-center gap-2"
+                  onClick={handleFilterButton}
+                >
                   <Image
                     src={filterIcon}
                     alt="Ícone do filtro"
                     className="w-5"
                   />
-                  Filtro
+                  <span className="hidden md:block">Filtro</span>
                 </button>
+
+                {/* Dropdown */}
+
+                {filterBtnOpen && <FilterButton />}
               </div>
             </div>
           )}
         </div>
 
         <div className="w-full h-auto flex flex-row flex-wrap justify-center items-start p-3 md:gap-10 gap-3">
-          {carregando ? (
-            Array.from({ length: 3 }).map((_, index) => (
-              <SkeletonCard key={index} />
-            ))
-          ) : eventos.length === 0 ? (
-            <div className="w-full h-40 flex justify-center items-center">
-              <h1 className="text-2xl font-bold text-slate-900">
-                Nenhum evento encontrado
-              </h1>
-            </div>
-          ) : (
-            eventos.map((evento) => (
-              <Link
-                href={`/eventos/${evento.id}`}
-                className="w-72 min-h-64 shadow flex flex-col rounded-2xl"
-                key={evento.id}
-              >
-                <div className="w-full h-[150px] bg-orange-500 rounded-t-2xl">
-                  <Image
-                    className="size-full object-cover rounded-t-2xl"
-                    src={evento.cover_photo_url}
-                    alt={evento.titulo}
-                    width={300}
-                    height={300}
-                  />
-                </div>
-
-                <div className="w-full flex flex-col justify-between items-start p-4 rounded-b-2xl">
-                  <div className="w-full h-auto mb-2">
-                    <h3 className="text-xs text-orange-500 text-left font-semibold capitalize mb-2">
-                      {evento.data}
-                    </h3>
-
-                    <h3 className="text-sm text-slate-900 text-left font-semibold capitalize mb-2">
-                      {evento.titulo}
-                    </h3>
-                  </div>
-
-                  <div className="w-full max-h-8">
-                    <h3 className="text-xs text-slate-900 text-left font-semibold capitalize">
-                      {evento.local}
-                    </h3>
-                  </div>
-                </div>
-              </Link>
-            ))
-          )}
+          {carregando ? renderSkeleton() : renderEventos()}
+          {carregando ? renderEventos() : renderNenhumEvento()}
         </div>
       </div>
     </main>
